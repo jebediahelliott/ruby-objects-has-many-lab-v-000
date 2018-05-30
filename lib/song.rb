cd2df4c054d4d18a23e8adb5a008ad7107a36870
Jebediah Elliott <jebediahelliott@gmail.com>
2018-05-30 20:32:20 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,16 +13,3 @@ class Song
 end
 
 
-class Post
-
-  attr_accessor :title, :author
-  @@all = []
-  def initialize(title)
-    @title = title
-    @@all << self
-  end
-
-  def author_name
-    self.author ? self.author.name : nil
-  end
-end
